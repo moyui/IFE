b6e4e7dded4969c03bb9f47b1707155946a70aa9
EgoistYiLuo <gxy1510@gmail.com>
2017-08-14 14:28:49 +0800

    task_32

--- task_32/script.js
+++ task_32/script.js
@@ -21,6 +21,7 @@ function Form(option) {
 	this.rule = option.rule;
 	this.success = option.success;
 	this.fail = option.fail;
+	this.identity = option.identity;
 	this.form;
 
 	this.create();
@@ -31,6 +32,7 @@ Form.prototype.create = function() {
 		label = document.createElement("label"),
 		input = document.createElement("input"),
 		hint = document.createElement("span"),
+		wrap = document.getElementById("wrapper");
 		self = this;
 
 	label.innerHTML = this.label;
@@ -39,6 +41,7 @@ Form.prototype.create = function() {
 	form.appendChild(label);
 	form.appendChild(input);
 	form.appendChild(hint);
+	wrap.qppendChild(form);
 
 	this.form = form;
 
@@ -54,5 +57,53 @@ Form.prototype.create = function() {
 };
 
 Form.prototype.validator = function() {
-	
-}
+	var form = this.form,
+		inputText = form.querySelector("input").trim(),
+		hint = form.querySelector("span"),
+		testText = inputText.replace(/[\u4E00-\uFA29]|[\uE7C7-\uE7F3]/g, "aa");
+
+		if(!testText) {
+			hint.innerHTML = form.querySelector("label").innerHTML + "不能为空";
+			hint.style.color = "#FA0F0F";
+			hint.style.borderColor = "#FA0F0F" ;
+		}	
+
+	switch(this.identity) {
+		case "username":
+		case "password": testLen() ? correct(this.success) : incorrect(this.false);break;
+		case "passwordTest": testPassword() ? correct(this.success) : incorrect(this.false);break;
+		case "mail": testMail() ? correct(this.success) : incorrect(this.form, this.fail);break;
+		case "phoneNumber" :testPhoneNumber() ? correct(this.success) : incorrect(this.false);break;
+	}
+
+    function testLen() {
+        return /^.{4,16}$/.test(testText);
+    }
+
+    function testPassword() {
+        var inputs = document.querySelectorAll("input");
+        return inputs[1].value == inputs[2].value;
+    }
+
+    function testMail() {
+        return /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(inputText);
+    }
+
+    function testPhoneNumber() {
+        return /^(13[0-9]|15[5-9]|15[0-3]|18[5-9]|180)[0-9]{8}$/.test(inputText);
+    }
+
+    function correct(hintWords) {
+    	var input = form.querySelector("input");
+        hint.innerHTML = hintWords;
+        hint.style.color = "#09F62F";
+        input.style.borderColor = "#09F62F";
+    }
+
+    function incorrect(hintWords) {
+    	var input = form.querySelector("input");
+        hint.innerHTML = hintWords;
+        hint.style.color = "#FA0F0F";
+        input.style.borderColor = "#FA0F0F";
+    } 
+};
