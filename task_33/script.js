6e094e455f70c7917ed6e92101f4cc0c954c6e71
EgoistYiLuo <gxy1510@gmail.com>
2017-08-18 20:18:12 +0800

    task_33

--- task_33/script.js
+++ task_33/script.js
@@ -16,6 +16,37 @@
 	}
 })();
 
-var chess = function() {
+function inputProcess() {
+	var input = document.getElementById("input"),
+		inputText = input.value.trim();
 
+}
+
+function Chess(option) {
+	this.x = option.x;
+	this.y = option.y;
+	this.towards = option.towards;
+	this.chess;
+	this.create();
+}
+
+Chess.prototype.create = function() {
+	var chess = document.createElement("span"),
+		y = document.querSelectorAll("tr")[this.y],
+		x = y.querySelectorAll("td")[this.x];
+	x.appendChild(chess);
+	this.chess = chess;
+};
+
+Chess.prototype.go = function() {
+	var self = this;
+	switch(this.towards){
+		case "front": this.y--; ; break;
+		case "back" : this.y++; ; break;
+		case "left" : this.x--; ; break;
+		case "right": this.x++; ; break;
+	}
+	function step() {
+		var chess = document.querySelecor("span");
+	}
 };
