1691878f303f16bbb33e7228d67c47c73adb84d9
moyui <gxy1510@gmail.com>
2017-10-07 23:29:07 +0800

    2017/10/7

--- IFE 2016/task_40mobile/script.js
+++ IFE 2016/task_40mobile/script.js
@@ -1,10 +1,14 @@
 function Calendar(config) {
     this.days = ['日', '一', '二', '三', '四', '五', '六'];
+    this.clientWidth;
 
     this.month;
     this.year;
     this.date;
-    this.firstDay;
+
+    this.nowYear;
+    this.nowMonth;
+    this.nowDate;
 
     this.calendar;
     this.calendarShow;
@@ -31,6 +35,9 @@ Calendar.prototype.init = function() {
         calendarTitle.className = 'calendarTitle';
         calendarShow.className = 'calendarShow';
 
+        this.clientWidth = document.documentElement.clientWidth;
+        document.documentElement.style.fontSize = this.clientWidth / 10.8 + 'px';
+
         calendarTitle.appendChild(cTtr);
         calendar.appendChild(calendarTitle);
         calendar.appendChild(calendarShow);
@@ -38,8 +45,10 @@ Calendar.prototype.init = function() {
         this.year = today.getFullYear();
         this.month = today.getMonth();
         this.date = today.getDate();
-        //计算本月第一天是星期几
-        this.firstDay = new Date(this.year, this.month, 1).getDay();
+        //保留今天日期
+        this.nowYear = today.getFullYear();
+        this.nowMonth = today.getMonth();
+        this.nowDate = today.getDate();
 
         this.calendar = calendar;
         this.calendarShow = calendarShow;
@@ -48,15 +57,16 @@ Calendar.prototype.init = function() {
         this.render();
 };
 
-Calendar.prototype.render = function() {
+Calendar.prototype.render = function() {              
+
     var calendarShow = this.calendarShow,
         fragment = document.createDocumentFragment(),
 
         month = this.month,
         year = this.year,
-
-        firstDay = this.firstDay,
-        monthNext = month + 1,
+        monthNext = month + 1;    
+        //计算本月第一天是星期几 
+    var firstDay = new Date(this.year, this.month, 1).getDay(),
         //计算上月空余几天
         lastDays = (new Date(year, month, 0)).getDate() - firstDay + 1,
         //计算当月总天数(最后一天)
@@ -64,7 +74,7 @@ Calendar.prototype.render = function() {
         //需要加载的日历行数
         renderTr = Math.floor((renderDays + firstDay)/7) + 1;
         
-    let count = 1,
+    var count = 1,
         countLast = 1;
 
     for (let i = 0; i < renderTr; i++) {
@@ -93,10 +103,6 @@ Calendar.prototype.render = function() {
     this.changeColor();
 };
 
-Calendar.prototype.bind = function() {
-
-};
-
 Calendar.prototype.changeColor = function() {
     var color = randomColor(),
 
@@ -107,17 +113,61 @@ Calendar.prototype.changeColor = function() {
     calendarHead.style.backgroundColor = color;
 };
 
+Calendar.prototype.bind = function() {
+
+};
+
 Calendar.prototype.slide = function(event) {
-    var touch = event.targetTouches[0];
+    var touch = event.targetTouches[0],
+        self = this,        
+        calendarShow = this.calendarShow,
+        clientWidth = this.clientWidth,
+        ways;
+
+    var startPos = {
+            x: touch.pageX,
+            y: touch.pageY
+        };
 
     var process = {
-        touchMove: function() {
-            
+        touchMove: function(event) {
+            var endPos = {
+                x: touch.pageX - startPos.x,
+                y: touch.pageY - startPos.y
+            };
+            //1表示右划，0表示左划
+            ways = (endPos.y > 0) ? 1 : 0;
+            //计算滑动距离
+            event.preventDefault();
         },
+
+        touchEnd: function(event) {
+            var endPos = {
+                x: touch.pageX - startPos.x,
+                y: touch.pageY - startPos.y
+            },
+            absDistant = Math.abs(endPos.y);
+            if ((ways === 1) && absDistant >= (clientWidth / 5)) {
+                let newDate = new Date(self.year, self.month + 1, 1);
+                //计算新的render时间
+                self.year = newDate.getFullYear();
+                self.month = newDate.getMonth();
+                self.date = newDate.getDate();
+            } else if ((ways === 0) && absDistant >= (clientWidth / 5)) {
+                let newDate = new Date(self.year, self.month - 1, 1);
+                //计算新的render时间
+                self.year = newDate.getFullYear();
+                self.month = newDate.getMonth();
+                self.date = newDate.getDate();
+                removeEvent(calendarShow, 'touchstart', self.slide);
+                removeEvent(calendarShow, 'touchmove', process.touchmove);
+                
+            }
+        }
     };
 };
 
 (function main() {    
-    document.documentElement.style.fontSize = document.documentElement.clientWidth / 10.8 + 'px';
     var calendar = new Calendar();
+    console.log(calendar);
 })();
